998a82b5f64d1c8c4489eb5e1a56971b4d9ea9bf
rupeshbabu <nrupesh08@gmail.com>
2025-04-25 18:52:17 +0530

    implement geo-code and dump json to db

--- models/dev.model.js
+++ models/dev.model.js
@@ -1,97 +1,126 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const slugify = require("slugify");
+const geoCoder = require("../utils/geoCoder");
 
 const DevSchema = new mongoose.Schema({
-    name: {
-        type:String,
-        required: [true, 'Please enter name'],
-        unique:true,
-        trim: true,
-        maxlength: [50, 'Name can not be more than 50 characters' ]
+  name: {
+    type: String,
+    required: [true, "Please enter name"],
+    unique: true,
+    trim: true,
+    maxlength: [50, "Name can not be more than 50 characters"],
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, "Please enter dscription"],
+    maxlength: [5000, "Description can not be more than 500 characters"],
+  },
+  website: {
+    type: String,
+    match: [
+      /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/,
+      "Please use a valid URL with HTTP or HTTPS",
+    ],
+  },
+  phone: {
+    type: String,
+    maxlength: [10, "Phone Number can not be longer than 10 charaters"],
+  },
+  email: {
+    type: String,
+    match: [/^\S+@\S+\.\S+$/, "Please enter valid email address"],
+  },
+  address: {
+    type: String,
+    required: [true, "Please add an address"],
+  },
+  location: {
+    // GeoJSON Point
+    type: {
+      type: String,
+      enum: ["Point"],
     },
-    slug: String,
-    description:{
-        type:String,
-        required: [true, 'Please enter dscription'],
-        maxlength: [5000, 'Description can not be more than 500 characters' ]
+    coordinates: {
+      type: [Number],
+      index: "2dsphere",
     },
-    website: {
-        type:String,
-        match:[/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/, 'Please use a valid URL with HTTP or HTTPS']
-    },
-    phone: {
-        type: String,
-        maxlength:[10, 'Phone Number can not be longer than 10 charaters']
-    },
-    email: {
-        type: String,
-        match:[/^\S+@\S+\.\S+$/, 'Please enter valid email address']
-    },
-    address: {
-        type: String,
-        required: [true, 'Please add an address']
-    },
-    location: {
-        // GeoJSON Point
-        type:{
-            type: String,
-            enum: ['Point'],
-            // required: true
-        },
-        coordinates: {
-            type:[Number],
-            // required: true,
-            index: '2dsphere'
-        },
-        formattedAddress: String,
-        street: String,
-        city: String,
-        state: String,
-        zipcode: String,
-        country: String,
-    },
-    careers:{
-        //Array of strings
-        type:[String],
-        required: true,
-        enum: [
-            'Web Development',
-            'Mobile Development',
-            'UI/UX',
-            'Data Science',
-            'Business',
-            'Other'
-        ]
-    },
-    averageRating: {
-        type: Number,
-        min: [1, 'Rating must be atleast 1'],
-        max: [10, 'Rating mut can not be more than 10']
-    },
-    averageCost: Number,
-    photo:{
-        type: String,
-        default:'no-photo.jpg'
-    },
-    housing: {
-        type: Boolean,
-        default: false
-    },
-    jobAssistance: {
-        type: Boolean,
-        default: false
-    },
-    jobGuarantee:{
-        type: Boolean,
-        default: false
-    },
-    acceptGi:{
-        type: Boolean,
-        default: false
-    },
-    createdAt:{
-        type: Date,
-        default: Date.now
-    }
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
+  careers: {
+    //Array of strings
+    type: [String],
+    required: true,
+    enum: [
+      "Web Development",
+      "Mobile Development",
+      "UI/UX",
+      "Data Science",
+      "Business",
+      "Other",
+    ],
+  },
+  averageRating: {
+    type: Number,
+    min: [1, "Rating must be atleast 1"],
+    max: [10, "Rating mut can not be more than 10"],
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: "no-photo.jpg",
+  },
+  housing: {
+    type: Boolean,
+    default: false,
+  },
+  jobAssistance: {
+    type: Boolean,
+    default: false,
+  },
+  jobGuarantee: {
+    type: Boolean,
+    default: false,
+  },
+  acceptGi: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('Dev', DevSchema);
+//Create dev slug from the name
+DevSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+//Geocode & create location field
+DevSchema.pre("save", async function (next) {
+  const loc = await geoCoder.geocode(this.address);
+
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formatted,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].state,
+    zipcode: loc[0].postcode,
+    country: loc[0].countryCode,
+  };
+
+  //Do not save address in DB
+  this.address = undefined;
+  next();
+});
+
+module.exports = mongoose.model("Dev", DevSchema);
---
+++ seeder.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors')
+const dotenv = require('dotenv')
+
+
+// Load env vars
+dotenv.config({path: './config/config.env'});
+
+//Load Models
+const Dev = require('./models/dev.model');
+
+//Connect DB
+mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+});
+
+//Read JSON
+const dev = JSON.parse(fs.readFileSync(`${__dirname}/_data/dev.json`, 'utf-8'));
+
+// Import into DB
+const importData = async() => {
+    try {        
+        await Dev.create(dev);
+        console.log("Data imported...".green.inverse);
+        process.exit();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+//Delete data
+const deleteData = async() => {
+    try {
+        await Dev.deleteMany();
+        console.log("Data deleted...".red.inverse);
+        process.exit();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+if(process.argv[2] === '-i'){
+    importData();
+}else if(process.argv[2] === '-d'){
+    deleteData();
+}
